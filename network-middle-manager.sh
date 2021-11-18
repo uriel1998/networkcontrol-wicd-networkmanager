5a2ed641198734c5ca3eaf828fb6729943894786
Steven Saus <steven@stevesaus.com>
2021-11-18 11:38:03 -0500

    flow more efficient unlike other middle managers...

--- network-middle-manager.sh
+++ network-middle-manager.sh
@@ -6,3 +6,12 @@
 #   Operates in USERSPACE after shedding root privileges
 #
 ########################################################################
+
+# should be put in /usr/local/bin/....
+# $1 is up/down
+# use wan_detect to feed current connection information into this script 
+# ensure is not already running!!! Only one copy running at a time!
+# if down, read ini for down conditions, do it, and exit
+# if up, see if any of the network attributes are in "trusted"
+# if not, execute "untrusted" 
+# otherwise execute "trusted"
